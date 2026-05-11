2f159633cad54ba2bb7f2c718fec4e5eacefb479
naveena0721 <naveena1bavishsai@gmail.com>
2026-05-11 23:31:54 +0530

    Final chit fund project completed

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request, redirect, send_from_directory
 
 app = Flask(__name__)
 
+@app.route('/frontend/<path:path>')
+def send_frontend(path):
+    return send_from_directory('frontend', path)
+
 @app.route('/')
 def home():
-    return "Chit Fund System Backend Running 🚀"
+    return redirect('/frontend/login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+
+    role = request.form['role']
+
+    if role == 'admin':
+        return redirect('/frontend/admin-dashboard.html')
+
+    else:
+        return redirect('/frontend/user-dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
